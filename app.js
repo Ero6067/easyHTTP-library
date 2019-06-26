7ed292f3becb8aeb7c69ce30937d131beaa441b5
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-06-26 14:25:26 -0700

    adding POST and PUT requests

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const http = new easyHTTP;
 
-//GET posts
-http.get('https://jsonplaceholder.typicode.com/posts', 
-function(err, posts) {
-  if(err) {
-    console.log(err);
+// //GET posts
+// http.get('https://jsonplaceholder.typicode.com/posts', 
+// function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// //Get Single post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', 
+// function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+//POST request
+// http.post('https://jsonplaceholder.typicode.com/posts', 
+// data, function(err, post) {
+//   if( err) {
+//     console.log(err)
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post / PUT req
+http.put('https://jsonplaceholder.typicode.com/posts/1', 
+data, function(err, post) {
+  if( err) {
+    console.log(err)
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
+
